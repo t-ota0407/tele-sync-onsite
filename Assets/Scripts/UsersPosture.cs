9a4ab9f5bfe89a6dc1b22268b715fe3f5914bb2e
t-ota0407 <119516647+t-ota0407@users.noreply.github.com>
2025-04-28 19:28:50 +0900

    add project

---
+++ Assets/Scripts/UsersPosture.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UsersPosture
+{
+    public Posture head;
+    public Posture leftHand;
+    public Posture rightHand;
+}
